675f67096676857179f1b259c0f3628a6bfcfcb1
Luiz Freneda <lfreneda@gmail.com>
2013-08-29 17:09:41 -0300

    Refactory CieloResponse, using awesomely extensions

--- src/Cielo/Responses/CieloResponse.cs
+++ src/Cielo/Responses/CieloResponse.cs
@@ -22,30 +22,12 @@ namespace Cielo.Responses
             var node = _xdocument.Descendants(XName.Get(xmlNodeName, "http://ecommerce.cbmp.com.br")).FirstOrDefault();
             if (node == null) return;
 
-            var left = GetMemberInfo(propertyExpression);
+            var left = propertyExpression.GetMember();
+            if (left == null) return;
             var propertyName = left.Member.Name;
 
             var value = (converter != null) ? converter.Convert(node.Value) : node.Value;
             this.SetPropertyValue(value, propertyName);
-        }
-
-        private static MemberExpression GetMemberInfo(Expression method)
-        {
-            var lambda = method as LambdaExpression;
-            if (lambda == null) throw new ArgumentNullException("method");
-
-            MemberExpression memberExpr = null;
-            if (lambda.Body.NodeType == ExpressionType.Convert)
-            {
-                memberExpr = ((UnaryExpression)lambda.Body).Operand as MemberExpression;
-            }
-            else if (lambda.Body.NodeType == ExpressionType.MemberAccess)
-            {
-                memberExpr = lambda.Body as MemberExpression;
-            }
-
-            if (memberExpr == null) throw new ArgumentException("method");
-            return memberExpr;
         }
     }
 }
