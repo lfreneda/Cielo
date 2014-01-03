54fb6def847f048a5f2039d8fa3f7803c4041a59
Luiz Freneda <lfreneda@gmail.com>
2014-01-03 10:38:13 -0200

    Handling Errors

--- src/Cielo/CieloService.cs
+++ src/Cielo/CieloService.cs
@@ -1,25 +1,22 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data;
 using Cielo.Responses;
 using Cielo.Requests;
 using Cielo.Responses.Exceptions;
 using RestSharp;
 
-namespace Cielo
-{
-    public class CieloService : ICieloService
-    {
+namespace Cielo {
+    public class CieloService : ICieloService {
         private readonly string _endPointUrl;
 
-        public CieloService(string endPointUrl = null)
-        {
+        public CieloService(string endPointUrl = null) {
             if (string.IsNullOrEmpty(endPointUrl)) endPointUrl = ConfigurationManager.AppSettings["cielo.webservice.url"];
             if (string.IsNullOrEmpty(endPointUrl)) throw new NoNullAllowedException("Cielo service endpoint was not provided and its not configured");
             _endPointUrl = endPointUrl;
         }
 
-        protected virtual string Execute(ICieloRequest cieloRequest)
-        {
+        protected virtual string Execute(ICieloRequest cieloRequest) {
             var client = new RestClient(_endPointUrl);
             var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Xml };
             var mensagemValue = cieloRequest.ToXml(false);
@@ -28,24 +25,34 @@ namespace Cielo
             return response.Content;
         }
 
-        public CreateTransactionResponse CreateTransaction(CreateTransactionRequest request)
-        {
-            var responseContent = Execute(request);
-            CreateExceptionIfError(responseContent);
-            return new CreateTransactionResponse(responseContent);
+        public CreateTransactionResponse CreateTransaction(CreateTransactionRequest request) {
+            var responseContent = string.Empty;
+
+            try {
+                responseContent = Execute(request);
+                CreateExceptionIfError(responseContent);
+                return new CreateTransactionResponse(responseContent);
+            }
+            catch (Exception ex) {
+                throw new ResponseException(responseContent, ex);
+            }
         }
 
-        public CheckTransactionResponse CheckTransaction(CheckTransactionRequest request)
-        {
-            var responseContent = Execute(request);
-            CreateExceptionIfError(responseContent);
-            return new CheckTransactionResponse(responseContent);
+        public CheckTransactionResponse CheckTransaction(CheckTransactionRequest request) {
+
+            var responseContent = string.Empty;
+            try {
+                responseContent = Execute(request);
+                CreateExceptionIfError(responseContent);
+                return new CheckTransactionResponse(responseContent);
+            }
+            catch (Exception ex) {
+                throw new ResponseException(responseContent, ex);
+            }
         }
 
-        private static void CreateExceptionIfError(string responseContent)
-        {
-            if (responseContent.Contains("<erro xmlns=\"http://ecommerce.cbmp.com.br\">"))
-            {
+        private static void CreateExceptionIfError(string responseContent) {
+            if (responseContent.Contains("<erro xmlns=\"http://ecommerce.cbmp.com.br\">")) {
                 throw new ResponseException(new ErrorResponse(responseContent));
             }
         }
--- src/Cielo/Responses/Exceptions/ResponseException.cs
+++ src/Cielo/Responses/Exceptions/ResponseException.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 
-namespace Cielo.Responses.Exceptions
-{
-    public class ResponseException : ApplicationException
-    {
+namespace Cielo.Responses.Exceptions {
+
+    public class ResponseException : ApplicationException {
         public ErrorResponse ResponseError { get; set; }
 
         public ResponseException(ErrorResponse responseError)
-            : base(responseError.Message)
-        {
+            : base(responseError.Message) {
             ResponseError = responseError;
+        }
+
+        public ResponseException(string responseContent, Exception innerException)
+            : base(message: responseContent, innerException: innerException) {
+
         }
     }
 }
